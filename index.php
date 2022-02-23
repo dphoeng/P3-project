cabdde5d66ba10eb0cab5b9886ea166ec01b3e73
Spyreex <329163@student.mboutrecht.nl>
2022-02-23 18:36:39 +0100

    committing changes for porting, navbar not working

--- index.php
+++ index.php
@@ -15,25 +15,27 @@
 <header>
 <nav>
         <div class="logo">
-            <h1><a href="index.html"></a>ES.Excido</h1>
+            <a href="./index.php"><img src="./src/img/dagblad_algemeen.png" alt=""></a>
         </div>
-        <ul class="nav-links">
-            <li><a href="">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#formulier">Score</a></li>
-            <li><a href="#contact-content">Contact</a></li>
+		<div class="menu">
+        <ul>
+            <li><a href="#sport">Sport</a></li>
+            <li><a href="#politiek">Politiek</a></li>
+            <li><a href="#economie">Economie</a></li>
+            <li><a href="#tech">Tech</a></li>
          <?php
             if (isset($_SESSION["userid"])) {
-                echo "<li><a href='./src/php/logout.php'>Logout</a></li>";
+                echo "<li><a href='./pages/logout.php'>Logout</a></li>";
             if (isset($_SESSION["userRole"]) && $_SESSION["userRole"] == 1)
-                 echo "<li><a href='./src/php/reservation.php'>Reservation</a></li>";
-                 echo "<li><a href='src/php/dashboard.php'>Dashboard</a></li>";
+                 echo "<li><a href='./pages/reservation.php'>Reservation</a></li>";
+                 echo "<li><a href='./pages/dashboard.php'>Dashboard</a></li>";
            } else {
-                echo "<li><a href='./src/php/signup.php'>Sign up</a></li>";
-                echo "<li><a href='./src/php/login.php'>Log in</a></li>";
+                echo "<li><a href='./pages/signup.php'>Sign up</a></li>";
+                echo "<li><a href='./pages/login.php'>Log in</a></li>";
             }
          ?>
         </ul>
+		</div>
         <div class="mobile-hamburger">
             <button class="hamburger hamburger--arrow" type="button">
                     <span class="hamburger-box">
@@ -51,7 +53,9 @@
 		echo "<button><a href=\"./pages/signup.php\">Registreren</a></button>";
 	}
 	?>
+<main>
 
+</main>
 </body>
 <script src="src/js/nav.js"></script>
 </html>
