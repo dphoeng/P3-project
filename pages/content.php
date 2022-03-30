a954589bd48dd823cabe30ebff3191d2ecae11b1
dphoeng <329163@student.mboutrecht.nl>
2022-03-31 00:37:47 +0200

    Completely revamped content.php, now it's a lot friendlier for adding additional catagories if needed. Additionally fixed the issue where if catagories had no articles, php would error

--- pages/content.php
+++ pages/content.php
@@ -1,144 +1,112 @@
 <?php
 
-for ($x = 0; $x < 4; $x++) {
-    $titleArray = array();
-    $imageArray = array();
-    $sql = 'SELECT * FROM artikelen WHERE sendToMain = 1 AND categorie = ' . $x . ' ORDER BY datum DESC LIMIT 5';
-    $result = $conn->query($sql);
+// dictionary for determining catargory
+$dictionary = [
+    0 => "Sport",
+    1 => "Politiek",
+    2 => "Economie",
+    3 => "Tech"
+];
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            array_push($titleArray, $row["titel"]);
-            array_push($imageArray, $row["imageLocation"]);
-        }
+// class for each catagory, which has an array for all the articles with that catagory
+class CardInfo
+{
+    public $category;
+    public $articleArray;
+
+    function __construct($category)
+    {
+        $this->category = $category;
+        $this->articleArray = array();
     }
-    switch ($x) {
-        case 0:
-            $titleArraySport = $titleArray;
-            $imageArraySport = $imageArray;
-            break;
-        case 1:
-            $titleArrayPolitiek = $titleArray;
-            $imageArrayPolitiek = $imageArray;
-            break;
-        case 2:
-            $titleArrayEconomie = $titleArray;
-            $imageArrayEconomie = $imageArray;
-            break;
-        case 3:
-            $titleArrayTech = $titleArray;
-            $imageArrayTech = $imageArray;
-            break;
+
+    public function addToArray($article)
+    {
+        array_push($this->articleArray, $article);
     }
 }
 
-?>
+// article class
+class Article
+{
+    public $title;
+    public $imageLocation;
 
+    function __construct($title, $imageLocation)
+    {
+        $this->title = $title;
+        $this->imageLocation = $imageLocation;
+    }
+}
+
+// initialize array where all the different cards go into
+$cards = array();
+
+// sql for finding how many catagories there are
+$sql = 'SELECT DISTINCT categorie FROM artikelen WHERE sendToMain = 1';
+$result = $conn->query($sql);
+$categoriesCount = $result->num_rows;
+
+$categoryList = [];
+
+// find all category (numbers) and place them in a list
+if ($result->num_rows > 0)
+{
+    while ($category = $result->fetch_assoc()) {
+        array_push($categoryList, $category['categorie']);
+    }
+}
+
+// add catagory cards to cards array
+for ($y = 0; $y < $categoriesCount; $y++)
+{
+    array_push($cards, new CardInfo($dictionary[$categoryList[$y]]));
+}
+
+// loop through each catagory that has an article and find up to 5 articles and put them into an array in the right card
+for ($x = 0; $x < $categoriesCount; $x++)
+{
+    $sql = 'SELECT * FROM artikelen WHERE sendToMain = 1 AND categorie = ' . $categoryList[$x] . ' ORDER BY datum DESC LIMIT 5';
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $cards[$x]->addToArray(new Article($row['titel'],$row['imageLocation']));
+        }
+    }
+}
+?>
 
 <main>
     <h1 class="banner">Dagblad Algemeen</h1>
-    <div class="container" id="sport">
-        <div class="card">
-            <h2>Sport</h2>
-            <div class="row">
-                <div class="col main-article <?php if (isset($_SESSION["userid"])) { echo "logged-in";} ?>">
-                    <img src="<?php echo $imageArraySport[0]; ?>">
-                    <span><?php echo $titleArraySport[0]; ?></span>
-                </div>
-                <?php if (isset($_SESSION["userid"])) {
+
+    <!-- loops through all the catagory cards and creates cards out of them -->
+    <?php foreach ($cards as $card) {
+        echo '<div class="container" id="' . strtolower($card->category). '">' . 
+                '<div class="card">' .
+                    '<h2>' . $card->category . '</h2>' .
+                    '<div class="row">' .
+                        '<div class="col main-article '; if (isset($_SESSION["userid"]) && count($card->articleArray) > 1) { echo "logged-in";} echo '">' .
+                            '<img src="'; echo $card->articleArray[0]->imageLocation; echo '">' .
+                            '<span>' . $card->articleArray[0]->title . '</span>' .
+                        '</div>';
+                if (isset($_SESSION["userid"]) && count($card->articleArray) > 1) {
                     echo '<div class="col side-articles">';
-                    for ($i = 1; $i < count($titleArraySport); $i++) {
-                        echo
-                            '<div class="row">
+                    for ($i = 1; $i < count($card->articleArray); $i++) {
+                        echo '<div class="row">
                                 <div class="col article">
-                                    <img src="'.$imageArraySport[$i].'">
+                                    <img src="'.$card->articleArray[$i]->imageLocation.'">
                                 </div>
                                 <div class="col article-text">
-                                    <p>' . $titleArraySport[$i] . '</p>
+                                    <p>' . $card->articleArray[$i]->title . '</p>
                                 </div>
                             </div>';
                     }
                     echo '</div>';
-                } ?>
-            </div>
-        </div>
-    </div>
-    <div class="container" id="politiek">
-        <div class="card">
-            <h2>Politiek</h2>
-            <div class="row">
-                <div class="col main-article <?php if (isset($_SESSION["userid"])) { echo "logged-in";} ?>">
-                    <img src="<?php echo $imageArrayPolitiek[0]; ?>">
-                    <span><?php echo $titleArrayPolitiek[0]; ?></span>
-                </div>
-                <?php if (isset($_SESSION["userid"])) {
-                    echo '<div class="col side-articles">';
-                    for ($i = 1; $i < count($titleArrayPolitiek); $i++) {
-                        echo
-                            '<div class="row">
-                                <div class="col article">
-                                    <img src="'.$imageArrayPolitiek[$i].'">
-                                </div>
-                                <div class="col article-text">
-                                    <p>' . $titleArrayPolitiek[$i] . '</p>
-                                </div>
-                            </div>';
-                    }
-                    echo '</div>';
-                } ?>
-            </div>
-        </div>
-    </div>
-    <div class="container" id="economie">
-        <div class="card">
-            <h2>Economie</h2>
-            <div class="row">
-                <div class="col main-article <?php if (isset($_SESSION["userid"])) { echo "logged-in";} ?>">
-                    <img src="<?php echo $imageArrayEconomie[0]; ?>">
-                    <span><?php echo $titleArrayEconomie[0]; ?></span>
-                </div>
-                <?php if (isset($_SESSION["userid"])) {
-                    echo '<div class="col side-articles">';
-                    for ($i = 1; $i < count($titleArrayEconomie); $i++) {
-                        echo
-                            '<div class="row">
-                                <div class="col article">
-                                    <img src="'.$imageArrayEconomie[$i].'">
-                                </div>
-                                <div class="col article-text">
-                                    <p>' . $titleArrayEconomie[$i] . '</p>
-                                </div>
-                            </div>';
-                    }
-                    echo '</div>';
-                } ?>
-            </div>
-        </div>
-    </div>
-    <div class="container" id="tech">
-        <div class="card">
-            <h2>Tech</h2>
-            <div class="row">
-                <div class="col main-article <?php if (isset($_SESSION["userid"])) { echo "logged-in";} ?>">
-                    <img src="<?php echo $imageArrayTech[0]; ?>">
-                    <span><?php echo $titleArrayTech[0]; ?></span>
-                </div>
-                <?php if (isset($_SESSION["userid"])) {
-                    echo '<div class="col side-articles">';
-                    for ($i = 1; $i < count($titleArrayTech); $i++) {
-                        echo
-                            '<div class="row">
-                                <div class="col article">
-                                    <img src="'.$imageArrayTech[$i].'">
-                                </div>
-                                <div class="col article-text">
-                                    <p>' . $titleArrayTech[$i] . '</p>
-                                </div>
-                            </div>';
-                    }
-                    echo '</div>';
-                } ?>
-            </div>
-        </div>
-    </div>
+                }
+                echo '</div>' .
+                '</div>' .
+            '</div>';
+    }
+    ?>
+
 </main>
